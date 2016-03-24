bfc83c096a2fd6a133d4c86865270239f365a827
Jessica Tran Hur <jesshur824@gmail.com>
2016-03-24 15:23:31 -0500

    add jshint, prepare webpack build to output to public/ in server/

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,19 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const DefinePlugin = require('webpack').DefinePlugin;
+const ProvidePlugin = webpack.ProvidePlugin;
+
 module.exports = {
   entry: [
     './src/App.jsx'
   ],
   output: {
-    path: __dirname,
-    filename: "bundle.js"
+    path: path.resolve(__dirname, '../server/public'),
+    filename: 'bundle.js'
   },
+  plugins: [
+        new HtmlWebpackPlugin({ //DO NOT USE 'PLUGIN'. THE 'S' is important!
+            template: './index.html'
+        })
+  ],
   module: {
+    preloaders: [{
+        test: /\.js$/, // include .js files
+        exclude: /node_modules/, // exclude any and all files in the node_modules folder
+        loader: "jshint-loader"
+    }],
     loaders: [{
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        loader: 'babel',
       // test: /\.jsx?$/,
       // exclude: /node_modules/,
       // loader: 'babel'
+      query: {
+        presets: ['es2015']
+      }
     }]
   }
 };
