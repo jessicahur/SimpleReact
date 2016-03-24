4c199b4a39b6a38f91b6a865e236c2b71fcf754c
Jessica Tran Hur <jesshur824@gmail.com>
2016-03-24 14:41:59 -0500

    props.children

--- OldJs/old.js
+++ OldJs/old.js
@@ -1,4 +1,6 @@
-/* Hello World */
+/**********************************
+*          Hello World            *
+**********************************/
 
 import React, { Component} from 'react';
 import { render } from 'react-dom'; //Need this because React.render is deprecated
@@ -13,3 +15,42 @@ class Hello extends Component {
 }
 
 render(<Hello />, document.getElementById('root'));
+
+/*********************************
+*     Parent - Child Components  *
+*********************************/
+
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+//Parent Component
+class GroceryList extends Component {
+  render() {
+    return (
+      <ul>
+        <ListItem quantity = '1' name = 'Bread' />
+        <ListItem quantity = '6' name = 'Eggs' />
+        <ListItem quantity = '2' name = 'Milk' />
+      </ul>
+      );
+  }
+}
+
+//Child Component
+class ListItem extends Component {
+  render () {
+    return (
+        <li>
+          { this.props.quantity } x {this.props.name}
+        </li>
+      );
+  }
+}
+
+render(<GroceryList />, document.getElementById('root'));
+
+/* Notes:
+- ListItem (which is child component) is reusable with different values of quantities and names.
+- Child elements can be configured through "props": the properties owned and passed down by the parent component.
+- No need to define props separately in the parent component.
+
--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ class GroceryList extends Component {
   render() {
     return (
       <ul>
-        <ListItem quantity = '1' name = 'Bread' />
-        <ListItem quantity = '6' name = 'Eggs' />
-        <ListItem quantity = '2' name = 'Milk' />
+        <ListItem quantity = '1'>Bread</ListItem>
+        <ListItem quantity = '6'>Eggs</ListItem>
+        <ListItem quantity = '2'>Milk</ListItem>
       </ul>
       );
   }
@@ -19,7 +19,7 @@ class ListItem extends Component {
   render () {
     return (
         <li>
-          { this.props.quantity } x {this.props.name}
+          { this.props.quantity } x {this.props.children}
         </li>
       );
   }
@@ -28,6 +28,5 @@ class ListItem extends Component {
 render(<GroceryList />, document.getElementById('root'));
 
 /* Notes:
-- ListItem (which is child component) is reusable with different values of quantities and names.
-- Child elements can be configured through "props": the properties owned and passed down by the parent component.
-- No need to define props separately in the parent component.
+- Besides using "named" props, it's possible to reference the content between the OPENING and CLOSING tags in the parent component using "props.children"
+*/
