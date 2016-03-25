39a50fb3d1372a3f708db5a67d314aaa29b4dddd
Jessica Tran Hur <jesshur824@gmail.com>
2016-03-25 15:00:53 -0500

    KanbanBoard, List components

---
+++ client/src/components/CheckList.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class CheckList extends Component {
+  render() {
+    let tasks = this.props.tasks.map(task => {
+      <li className="checklist_task">
+    })
+  }
+}
