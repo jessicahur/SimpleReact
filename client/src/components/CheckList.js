9172bece7cef7f3c6f5e52def031daf6b6429878
Jessica Tran Hur <jesshur824@gmail.com>
2016-03-25 20:34:07 -0500

    have checklist rendered but stil not rid of warning msg

--- client/src/components/CheckList.js
+++ client/src/components/CheckList.js
@@ -4,11 +4,11 @@ class CheckList extends Component {
   render() {
         console.log('At Checklist', this.props.tasks);
     let tasksM = this.props.tasks.map(task => { //Use "let" here so that we don't override the (var) tasks that got passed down from Card?
-      <p className="checklist_task" >
-        <input type='checkbox' defaultChecked={task.done} key={task.id}/>
+      return (<p className="checklist_task" key={task.id}>
+        <input type='checkbox' defaultChecked={task.done} />
         {task.name}
         <a href='#' className='checklist__task--remove' />
-      </p>
+      </p>);
     }); // after fat arrow used () ?
     return (
       <div className='checklist'>
